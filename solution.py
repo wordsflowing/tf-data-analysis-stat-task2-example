80b56788435047deaf979ae8158c54192aba0b34
wordsflowing <129582590+wordsflowing@users.noreply.github.com>
2023-04-04 17:28:05 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from scipy.stats import norm
 from scipy.stats import gamma
+from scipy.stats import expon
 
 
 chat_id = 422119389 # Ваш chat ID, не меняйте название переменной
@@ -11,6 +12,7 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    x = [ -x for x in x]
     alpha = 1 - p
-    return (gamma.ppf(alpha / 2, len(x),1) + len(x)*np.mean(x) - len(x)/2) / (len(x)/2*65**2), \
-           (gamma.ppf(1 - alpha / 2, len(x),1) + len(x)*np.mean(x) - len(x)/2) / (len(x)/2*65**2)
+    return (expon.ppf(alpha / 2, len(x)) - min(x) - 1/2) / (65**2 / 2), \
+           (expon.ppf(1 - alpha / 2, len(x)) - min(x) - 1/2) /(65**2 / 2)
