846ae05462870ad4192c4dd482cef2c1a64a692b
wordsflowing <129582590+wordsflowing@users.noreply.github.com>
2023-04-05 11:16:47 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,10 +8,13 @@ from scipy.stats import expon
 
 chat_id = 422119389 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.array) -> tuple:
+ef solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    return (gamma.ppf(alpha / 2, len(x)) + len(x)*np.mean(x) - len(x)/2) / (len(x)/2*65**2), \
-           (gamma.ppf(1 - alpha / 2, len(x)) + len(x)*np.mean(x) - len(x)/2) / (len(x)/2*65**2)
+    n = len(x)
+    left = (-min(-x) - 1 / 2) / (65**2 / 2)
+    right = (-np.log(alpha) / n -min(-x) - 1 / 2) / (65**2 / 2)
+    return left, \
+            right
